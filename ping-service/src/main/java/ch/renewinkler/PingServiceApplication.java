5bd50cc79d856117358edcdab8fb09677fe33232
René Winkler <rene.winkler@xovis.com>
2017-07-22 20:56:07 +0200

    different

--- ping-service/src/main/java/ch/renewinkler/PingServiceApplication.java
+++ ping-service/src/main/java/ch/renewinkler/PingServiceApplication.java
@@ -2,10 +2,8 @@ package ch.renewinkler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
-@RefreshScope
 public class PingServiceApplication {
 
 	public static void main(String[] args) {
