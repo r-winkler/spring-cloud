cf13135f74f523915fdf9778f2485e62d9e3fad3
René Winkler <rene.winkler@xovis.com>
2017-07-23 12:47:08 +0200

    add all

--- ping-service/src/main/java/ch/renewinkler/PingServiceApplication.java
+++ ping-service/src/main/java/ch/renewinkler/PingServiceApplication.java
@@ -3,9 +3,11 @@ package ch.renewinkler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@RefreshScope // refresh endpoint need a POST request
+@RefreshScope // refresh endpoint needs a POST request and authentication is required
+@EnableEurekaClient
 public class PingServiceApplication {
 
 	public static void main(String[] args) {
