cbbb7f58bcbadcc795c216dc78c63eed865b3224
René Winkler <rene.winkler@xovis.com>
2017-07-28 14:02:39 +0200

    Add authorization server

--- authorization-server/src/main/java/ch/renewinkler/AuthorizationServerApplication.java
+++ authorization-server/src/main/java/ch/renewinkler/AuthorizationServerApplication.java
@@ -2,11 +2,35 @@ package ch.renewinkler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 @SpringBootApplication
+@RestController
+@EnableResourceServer
+@EnableAuthorizationServer
 public class AuthorizationServerApplication {
+
+    @GetMapping("/user")
+    public Map<String, Object> user(OAuth2Authentication user) {
+        Map<String, Object> userInfo = new HashMap<>();
+        userInfo.put("user", user.getUserAuthentication().getPrincipal());
+        userInfo.put("authorities", AuthorityUtils.authorityListToSet(user.getUserAuthentication().getAuthorities()));
+        return userInfo;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationServerApplication.class, args);
     }
+
+
 }
+
